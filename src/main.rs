688c82c8d4b1babf1d34d8859f47c3c0002f698f
shunk-py <shun.kimura@integritis.io>
2022-12-17 19:57:31 +0900

    :sparkles: pass compile with parsing args by clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Args {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to raad
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    PrintIn("hello, world!")
+    let _args = Args::parse();
 }
